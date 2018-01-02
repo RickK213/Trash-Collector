d341564281fb4acd2932b47b641a81f00d2e8573
RickK213 <rkippert@att.net>
2018-01-02 16:33:26 -0600

    Finisished modifying Register and Manage methods

--- TrashCollector/Controllers/ManageController.cs
+++ TrashCollector/Controllers/ManageController.cs
@@ -70,7 +70,8 @@ namespace TrashCollector.Controllers
                 PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
                 TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
                 Logins = await UserManager.GetLoginsAsync(userId),
-                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
+                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId),
+                UserName = await UserManager.GetEmailAsync(userId)
             };
             return View(model);
         }
--- TrashCollector/Models/ManageViewModels.cs
+++ TrashCollector/Models/ManageViewModels.cs
@@ -12,6 +12,7 @@ namespace TrashCollector.Models
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+        public string UserName { get; set; }
     }
 
     public class ManageLoginsViewModel
